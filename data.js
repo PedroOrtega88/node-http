ab3ac82e5367f3c09d53f2ccbb9ead6a44eab808
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-01-11 19:59:40 +0100

    -a

--- data.js
+++ data.js
@@ -1,24 +1,14 @@
-
-
 const webpageData = {
-    title: "Tus Deportes de Riesgo",
-    subtitle: "Donde se encuentran los mejores deportes de riesgo",
-    description: "Si eres un amante de la adrenalina y te gustan los deportes de riesgo este es tu sitio.",
-    author: "Paco Pacheco",
-    year: 2024,
-    keywords: ["deportes de riesgo", "adrenalina", "extremo", "aventura"],
-    colorTheme: "white",
-    language: "es",
-    slogan: "¡Vive al límite!",
-    contactEmail: "info@deportesderiesgo.com",
-    enlacealatienda:"Tienda de Deportes de Riesgo",
-    socialLinks: {
-      twitter: "https://twitter.com/deportesderiesgo" + "Deportes de Riesgo",
-      facebook: "https://facebook.com/deportesderiesgo" + "Deportes de Riesgo",
-    },
-  };
-  
-  module.exports = webpageData;
-  
-  //node server.js
-  //http://localhost:3000
+  title: "Tus Deportes de Riesgo",
+  subtitle: "Donde se encuentran los mejores deportes de riesgo",
+  description: "Si eres un amante de la adrenalina y te gustan los deportes de riesgo este es tu sitio.",
+  author: "Paco Pacheco",
+  year: 2024,
+  keywords: ["deportes de riesgo", "adrenalina", "extremo", "aventura"],
+  language: "es",
+  slogan: "¡Vive al límite!",
+  contactEmail: "@deportesderiego.com",
+ 
+};
+
+module.exports = webpageData;
--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const http = require('http');
 const webpageData = require('./data');
 
-const server = http.createServer((req, res) => {
+let server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<!DOCTYPE html>');
   res.write('<html lang="' + webpageData.language + '">');
-  res.write('<head>');
-  res.write('<meta charset="UTF-8">');
   res.write(`<title>${webpageData.title}</title>`);
-  res.write('</head>');
-  res.write('<body style="background-color:' + webpageData.colorTheme.toLowerCase() + ';">');
   res.write(`<h1>${webpageData.title}</h1>`);
   res.write(`<h2>${webpageData.subtitle}</h2>`);
   res.write(`<p>${webpageData.description}</p>`);
@@ -17,17 +13,13 @@ const server = http.createServer((req, res) => {
   res.write(`<p>Año: ${webpageData.year}</p>`);
   res.write('<p>Slogan: ' + webpageData.slogan + '</p>');
   res.write('<p>Contacto: ' + webpageData.contactEmail + '</p>');
-  res.write('<p>Enlaces sociales:<br>');
-  res.write(`Twitter: <a href="${webpageData.socialLinks.twitter}">${webpageData.socialLinks.twitter}</a></p>`);
-  res.write(`Facebook: <a href="${webpageData.socialLinks.facebook}">${webpageData.socialLinks.facebook}</a></p>`);
-  res.write('<p>Palabras clave: ' + webpageData.keywords.join(', ') + '</p>');
   res.write('</body>');
   res.write('</html>');
-  res.end();
+  res.end('<h3>hola mundo</h3>');
 });
 
 const PORT = 3000;
 
-server.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+server.listen(3000, () => {
+  console.log(`server listening on port http://localhost:${PORT}`);
 });
